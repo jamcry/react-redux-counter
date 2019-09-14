65450d89ad7b6bb23a44acf7116ea6aec51deb57
jamcry <jamcry@hotmail.com>
2019-09-15 01:26:26 +0300

    Updates

---
+++ precache-manifest.227503a12019b0ac83ae516c3ff4a941.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bed0646b5492f8f769489e651accef8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae276f73096e3bb6afb4",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "32fda5a7ab5471bcf1df",
+    "url": "/static/js/2.a1162a9e.chunk.js"
+  },
+  {
+    "revision": "ae276f73096e3bb6afb4",
+    "url": "/static/js/main.9a5078a8.chunk.js"
+  },
+  {
+    "revision": "6d897e3cf2e6034e76f5",
+    "url": "/static/js/runtime~main.d0cbb125.js"
+  }
+]);
